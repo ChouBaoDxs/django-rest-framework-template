9b3870f901412c826fb833562810566ed5ec5da5
choubaodxs <826842697@qq.com>
2021-05-13 15:16:22 +0800

    CONN_MAX_AGE

--- code/drf_template/settings/base.py
+++ code/drf_template/settings/base.py
@@ -105,6 +105,7 @@ DATABASES = {
             'init_command': 'SET sql_mode=STRICT_TRANS_TABLES, innodb_strict_mode=1',
             'charset': 'utf8mb4',
         },
+        'CONN_MAX_AGE': 600  # 连接保持 600 秒，算是优化
     },
     # 'db2': {
     #     'ENGINE': 'django.db.backends.sqlite3',
