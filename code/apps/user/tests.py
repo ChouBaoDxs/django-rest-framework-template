44c2b979e3437a790f288debdc43f38d9d2c2a4e
choubaodxs <826842697@qq.com>
2021-07-02 15:14:53 +0800

    由于 apps 目录的存在，test 需要在最外面 import

--- code/apps/user/tests.py
+++ code/apps/user/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class UserTest(APITestCase):
+    def test_func1(self):
+        self.assertEqual(1, 1)
